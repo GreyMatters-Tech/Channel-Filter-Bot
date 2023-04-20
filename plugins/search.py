1839c7f8f2b30a9ab53550051aeaec6bab6067ff
GreyMatter's <greymattersyt@gmail.com>
2023-04-20 13:15:17 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -17,7 +17,7 @@ async def search(bot, message):
     if message.text.startswith("/"):
        return    
     query   = message.text 
-    head    = "<u>Here is the results 👇\n\nPowered By </u> <b><I>@CyniteBackup</I></b>\n\n"
+    head    = "<u>Here is the results 👇\n\nPromoted By </u> <b><I>@GreyMatter_Bots</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
@@ -57,7 +57,7 @@ async def recheck(bot, update):
     id      = update.data.split("_")[-1]
     query   = await search_imdb(id)
     channels = (await get_group(update.message.chat.id))["channels"]
-    head    = "<u>I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\nPowered By </u> <b><I>@CyniteBackup</I></b>\n\n"
+    head    = "<u>I Have Searched Movie With Wrong Spelling But Take care next time 👇\n\nPromoted By </u> <b><I>@GreyMatter_Bots</I></b>\n\n"
     results = ""
     try:
        for channel in channels:
