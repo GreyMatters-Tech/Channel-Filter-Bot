26ea9fba375666ba341159facd59ae98beee3d3c
GreyMatter's <greymattersyt@gmail.com>
2023-04-20 12:44:17 +0530

    Add files via upload

---
+++ utils/script.py
@@ -0,0 +1,40 @@
+class script(object):
+    START = """**Hello {} 🤟**
+   
+I am **Find Post Bot**.I am best Channel Link Search Bot! 
+I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+
+    HELP = """To Use me In A Group
+
+- Add me in your group & channel with all permissions. 
+- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @CyniteOfficial.
+- After verification send /connect YourChannelID
+- Example : /connect -100xxxxxxxxxx
+- Done ✅. <b><I>Enjoy 💜❤</I></b> )
+
+Remove a Channel with - /disconnect-100xxxxxxxxxxx
+this will help you to remove a indexed channel from your group.
+
+Get connected channels list with - /connections"""
+
+    ABOUT = """Powered By @CyniteBackup
+
+✯ Mʏ Nᴀᴍᴇ:  {}
+✯ Cʀᴇᴀᴛᴏʀ: <a href='tg://user?id=1883570185'>Harman</a>
+✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
+✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
+✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
+
+    STATS = """My Status 💫
+
+👥 Users: {}
+🧿 Groups: {}"""
+
+    BROADCAST = """<u>{}</u>
+
+Total: `{}`
+Remaining: `{}`
+Success: `{}`
+Failed: `{}`"""
+
+    
