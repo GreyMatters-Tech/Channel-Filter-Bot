960bba01b6b35229ced30b11dfb04ee45608da5a
subhan712 <118384635+subhan712@users.noreply.github.com>
2023-04-26 00:19:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 24355479
 API_HASH     = "db535209f86a2e961722cf66c81dbf95"
 BOT_TOKEN    = "5996922602:AAGh0swXSdzt_28bEJn68lyqLvjzqGqjwnc"
 SESSION      = "BQFzopcALKrhh8DIPL_lWTrcdZbngToK8bfMFsZ11S6UCxfLDh5aQ6IVV6sEGLEmbhQcSEYmNbczzgs2UiZqzWQuNYyzRw5tZu2c2RpzycyGpE9FvBqqf9Ll_FJ1u0F1oTRGBFEmlg0pkZeQWGrgvfnzRB25DKzwBzu1Qzkgq9erYTGgkzOOG7D9VrplNtGC1-wkylIesn_LcDa0oiO5ncjMtErmUzhp06l8duEWZzYyRjau1IVGKnL8f7DF_NFRyoPU0zLNPM5gaMithrW_OJgjkuFOg9EeUapYJuVbCA-YvM03xeS4YdGAbNomDCBy8mkrM2QjyAUgAwTWvIvHAWqSUiNlsQAAAAE9j6AXAA"
-DATABASE_URI = "mongodb+srv://sofiyan:sofiyan@cluster0.zktncoj.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://sofiyan71213:sofiyan@cluster0.zwpbwbg.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001884373226
 ADMIN        = 5327790103
 CHANNEL      = "@iPapkornbetabots"
