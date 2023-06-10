96b69777f7c588db38ef4ac061ba44003808a9a5
gamingsaha <85555051+gamingsaha@users.noreply.github.com>
2023-06-10 16:23:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 27448627
+API_HASH     = "518b7014ff26525da265f57b46514b03"
+BOT_TOKEN    = "6119262960:AAFsZXFZ2EVi8ULfI7alOxh2p4qJj3ptuBU"
+SESSION      = "BQCx7qA2ygv_z6nlsq67H7nM1V9LsdhSa6yTvXfugHRINe5J2dRiXFxC-tKYIB1aNMItFNoWtbG-R4yKxHiP0OohpnhHboIsFisJZO0kcOWG0jZ_BhS1w1YKqRoH9Q3l-m4lgeG5p2uOOlKO6rDu9-RTQe7otklucPzb89g6v6bxrYCFbF0pMblTCDrco1v29clI0jwBL-x2irnedgNmgLAsjJ_lSMwyLoFYFUuofhyz2S8mRcGpqpUZvSrD4gQoIrpQKq6_zQkpaHHJ16m63OjygKAfsr6gagTfur9UnHtFqPhuw3IUTEOIQgfkPTCQQfZqnrfcP3PsSymMifHuw-6NAAAAAT1-eBgA"
+DATABASE_URI = "mongodb+srv://SseriesArea:SseriesArea@cluster0.rjc4zzp.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001906981780
+ADMIN        = 5326665752
+CHANNEL      = "@Sseries_Area"
