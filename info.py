df54b9b96c16bc40eb87f999066d0f2ca4202904
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-07-22 20:30:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 25830285
 API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
-BOT_TOKEN    = "6575468895:AAEMLGs5nE89TmLwOZhUZYi9eICge9BneUo"
+BOT_TOKEN    = "5931458114:AAEGFZnSjsbw6Mlc7Tw149C2en76DpoPXwE"
 SESSION      = "BQGKI40AwawvpZtk71oauU9kev_oPsIbxu9Opw734HvCBryu3mhzeaTZ0kVsaK6BVMfSRLdbZ_5_B5rPOqbizUB2Aio0CNzkGzZsAQEB5r0YiIzSxIcEVLywLaINg9LDvaGYn8y2rFRPTjSnNDhO0J3jOUCKQKMhxeyCL9VRqaqqUi8tuZrNnPuCUQE1AzMB3gN4uMY3m7ZJN1_ai7GdgCH51Q8meexX4sv8vzKAzPyqa-VLLRG_MHifqdzh6_pxe3D7dRFzLt5Qkfwqyo64aZvGhiiYmozvNZYIYcemYnhGIOYFIE2qn5OWE8THUuo8AoZEpVmhUE_kYsb_CEeUnItvxwCHHgAAAAFrQhqtAA"
 DATABASE_URI = "mongodb+srv://hegodal811:obE9Ljvn0AHXbWeL@cluster0.lls1gnb.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001830551521
+LOG_CHANNEL  = -1001885390961
 ADMIN        = 6094461613
-CHANNEL      = "@GreyMatter_Bots"
+CHANNEL      = "@Movies_channel_7863"
