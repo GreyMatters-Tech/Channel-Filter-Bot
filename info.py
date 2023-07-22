5769e02bdcd6b67e9fa8a43108d2f1a535fa81ba
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-07-22 09:20:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 25830285
 API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
-BOT_TOKEN    = "6223100452:AAEs6uAxSPJzCh54bhAE-QZSMtuRoTEFEg8"
+BOT_TOKEN    = "6360916658:AAEoQrtSA0DlvzEPL1jZHSxGfZIHeNewlvw"
 SESSION      = "BQGKI40AwawvpZtk71oauU9kev_oPsIbxu9Opw734HvCBryu3mhzeaTZ0kVsaK6BVMfSRLdbZ_5_B5rPOqbizUB2Aio0CNzkGzZsAQEB5r0YiIzSxIcEVLywLaINg9LDvaGYn8y2rFRPTjSnNDhO0J3jOUCKQKMhxeyCL9VRqaqqUi8tuZrNnPuCUQE1AzMB3gN4uMY3m7ZJN1_ai7GdgCH51Q8meexX4sv8vzKAzPyqa-VLLRG_MHifqdzh6_pxe3D7dRFzLt5Qkfwqyo64aZvGhiiYmozvNZYIYcemYnhGIOYFIE2qn5OWE8THUuo8AoZEpVmhUE_kYsb_CEeUnItvxwCHHgAAAAFrQhqtAA"
 DATABASE_URI = "mongodb+srv://hegodal811:obE9Ljvn0AHXbWeL@cluster0.lls1gnb.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001944835958
