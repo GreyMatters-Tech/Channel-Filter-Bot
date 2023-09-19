c30913e75a1b3668779331b9ee4283bd899efd0e
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-19 21:39:26 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,31 +1,39 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hᴇʟʟᴏ👑 {} 👑**
    
-I am **MOVIES_VILLA_POST_FIND_BOT**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+I Aᴍ **Mᴏᴠɪᴇs Vɪʟʟᴀ Sᴇᴀʀᴄʜ Bᴏᴛ**. I Aᴍ Tʜᴇ Bᴇsᴛ Cʜᴀɴɴᴇʟ Lɪɴᴋ Sᴇᴀʀᴄʜ Bᴏᴛ! 
 
-    HELP = """To Use me In A Group
+I Wɪʟʟ Fɪʟᴛᴇʀ Yᴏᴜʀ Cʜᴀɴɴᴇʟ Pᴏsᴛ AᴜᴛᴏMᴀᴛɪᴄᴀʟʟʏ Aɴᴅ Sᴇɴᴅ Iᴛ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Wʜᴇɴ SᴏᴍᴇOɴᴇ Sᴇᴀʀᴄʜ Iᴛ."""
 
-- Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @ROYAL_GAGAN_DEEP_SINGH_MEHRA.
-- After verification send /connect YourChannelID
-- Example : /connect -100xxxxxxxxxx
-- Done ✅. <b><I>Enjoy 💜❤</I></b> )
+    HELP = """Tᴏ Usᴇ Mᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ
 
-Remove a Channel with - /disconnect-100xxxxxxxxxxx
-this will help you to remove a indexed channel from your group.
+- Aᴅᴅ Mᴇ Iɴ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Cʜᴀɴɴᴇʟ Wɪᴛʜ Aʟʟ Pᴇʀᴍɪssɪᴏɴs.
+ 
+- Sᴇɴᴅ /verify Iɴ Tʜᴇ Gʀᴏᴜᴘ Aɴᴅ Wᴀɪᴛ Fᴏʀ Iᴛ Tᴏ Aᴄᴄᴇᴘᴛ Oʀ Dɪʀᴇᴄᴛʟʏ Cᴏɴᴛᴀᴄᴛ Oᴡɴᴇʀ Aғᴛᴇʀ Rᴇǫᴜᴇsᴛ .
+@ROYAL_GAGAN_DEEP_SINGH_MEHRA
 
-Get connected channels list with - /connections"""
+- Aғᴛᴇʀ Vᴀʀɪғɪᴄᴀᴛɪᴏɴ Sᴇɴᴅ /connect YᴏᴜʀCʜᴀɴɴᴇʟIᴅ
 
-    ABOUT = """Developed By @ROYAL_GAGAN_DEEP_SINGH_MEHRA
+- Exᴀᴍᴘʟᴇ : /connect -100xxxxxxxxxx
+
+- Dᴏɴᴇ ✅. <b><I>♥️👑Eɴᴊᴏʏ 👑♥️</I></b> )
+
+Rᴇᴍᴏᴠᴇ Cʜᴀɴɴᴇʟ Iᴅ Wɪᴛʜ - /disconnect-100xxxxxxxxxxx
+Tʜɪs Wɪʟʟ Hᴇʟᴘ Yᴏᴜ Tᴏ Rᴇᴍᴏᴠᴇ Iɴᴅᴇxᴇᴅ Cʜᴀɴɴᴇʟ Fʀᴏᴍ Yᴏᴜʀ Gʀᴏᴜᴘ.
+
+Gᴇᴛ Cᴏɴɴᴇᴄᴛᴇᴅ Cʜᴀɴɴᴇʟ Lɪsᴛ Wɪᴛʜ - /connections
+
+- ᴘᴏᴡᴇʀᴇᴅ ʙʏ - @movies_villa_backup"""
+
+    ABOUT = """Dᴇᴠᴇʟᴏᴘᴇᴅ Bʏ @ROYAL_GAGAN_DEEP_SINGH_MEHRA
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/ROYAL_GAGAN_DEEP_SINGH_MEHRA'>👑ᴿᴼᵞᴬᴸ ᴼˢᴹ ᴹᴱᴴᴿᴬ👑</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/ROYAL_GAGAN_DEEP_SINGH_MEHRA'>👑Rᴏʏᴀʟ Osᴍ Mᴇʜʀᴀ👑</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
 
-    STATS = """My Status 💫
+    STATS = """Mʏ Sᴛᴀᴛᴜs 💫
 
 👥 Users: {}
 🧿 Groups: {}"""
