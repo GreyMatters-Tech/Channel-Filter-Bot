2635e285a88ec13f331eca28767d8305cb5c44ab
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2023-09-19 21:56:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 25830285
 API_HASH     = "0cbcabbf1b9c56a4dfc4b8950d584a2a"
-BOT_TOKEN    = "6320242599:AAEMKBv62_VwXP-Fn1fOOlVAM3KqIT6ni_w"
+BOT_TOKEN    = "6372645892:AAGCSvh6mDAQuzu01OW4S-k9G9mAJgJYH4w"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://Searchbot:Mongodb.bot@cluster0.5xixyjh.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001961336094
