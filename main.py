f3cb5236b8403614b54e9f30148de9165779d3e2
PREETMEHRA73638 <109457025+PREETMEHRA73638@users.noreply.github.com>
2023-11-14 17:14:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from client import Bot
-
+from pyrogram import Client, filters, idle
+from pyrogram.errors import QueryIdInvalid
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
+    InputTextMessageContent
+from plugins.
 print("Bot Started 💥")
 Bot().run()
 
