346d9d307fd7400ea93d1100da716ef2f14659dd
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2023-12-16 14:12:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,9 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 24335028
+API_HASH     = "b204ec833fb451fb913fc8e683b232d0"
+BOT_TOKEN    = "6776233446:AAGWmpBzm8O8Z-mYxGlbuQbs5ekelmxSDWU"
+SESSION      = "an online StringSession generat or[p]: Pyrogram (docs.pyrogram.or g)PyroGramBot ==> https://GitHub.
+com/SpEcHiDe/PyroGramBot[pv2]: Pyrogram (docs.pyrogram. org)PyroGramBot ==> https://docs.py rogram.org/releases/v2.0[t]: Telethon (docs.telethon.de v)Telethon UserBot ==> https://Gi tHub.com/SpEcHiDe/UniBorgSelect your required option:enter p/ pv2 / t ==>"
+DATABASE_URI = "mongodb+srv://lk3140318:yqduo05hHUIX15eU@cluster0.nygifov.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1002045377846
+ADMIN        = 5213073489
+CHANNEL      = "@FilmiChannelMix"
